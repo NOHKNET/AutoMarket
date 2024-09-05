a057f4cd602c1dfa34574cc366f4f1f3b0ace085
Le Van Khoa <levankhoa.work@gmail.com>
2024-09-05 11:17:56 +0700

    automarket build code

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,14 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            server: {
+                host: 'localhost',
+                https: true,
+                hmr: {
+                    host: '631f-113-164-236-59.ngrok-free.app',
+                    port: 443,
+                },
+            }
         }),
     ],
 });
